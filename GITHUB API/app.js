a8fd11f08e5f95e9c13c0d2d10e8c392ec48291f
Berkay <53509972+berkay9596@users.noreply.github.com>
2021-01-10 21:56:53 +0300

    Add files via upload

---
+++ GITHUB API/app.js
@@ -0,0 +1,63 @@
+// Element seçme
+
+const githubForm =document.getElementById("github-form");
+const nameInput = document.getElementById("githubname");
+const clearLastUsers = document.getElementById("clear-last-users");
+const lastUsers = document.getElementById("last-users");
+const github = new Github();
+const ui = new UI();
+
+eventListeners();
+function eventListeners(){
+    githubForm.addEventListener("submit",getData);
+    clearLastUsers.addEventListener("click",clearAllSearched);
+    document.addEventListener("DOMContentLoaded",getAllSearched);
+
+}
+
+function getData(e){
+let username = nameInput.value.trim();
+
+if(username===""){
+alert("Lütfen geçerli bir kullanıcı adı girin");
+}else{
+github.getGithubData(username)
+.then(response=>{
+    if(response.user.message === "Not Found"){
+        ui.showError("Kullanıcı Bulunamadı");
+    }else{
+        ui.addSearchedUserToUI(username);
+        Storage.addSearchedUserToStorage(username);
+        ui.showuserInfo(response.user);
+        ui.showRepoInfo(response.repo);
+    }
+})
+.catch(err=> ui.showError(err));
+}
+
+    ui.clearInput(); //input temizleme
+    e.preventDefault();
+}
+
+function clearAllSearched(){
+
+    if(confirm("Emin Misiniz?")){
+     
+        Storage.clearAllSearchedUsersFromStorage();
+        ui.clearAllSearchedUsersFromUI();
+
+    }
+
+}
+
+function getAllSearched(){
+    //arananları storegaden al ve ui a ekle
+
+    let users = Storage.getSearchedUsersFromStorage();
+    let result="";
+    users.forEach(user=>{
+        // <li class="list-group-item">asdaskdjkasjkşdjşasjd</li>
+        result+= `<li class="list-group-item">${user}</li>`;
+    })
+lastUsers.innerHTML=result;
+}
